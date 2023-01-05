d4967d43dcb8687ae308546c7494a06a136c3606
F1dg3t <57208077+dramaticrobotic@users.noreply.github.com>
2023-01-04 16:32:04 -0800

    Upload text and css

---
+++ rules.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/animations.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro">
+
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+</head>
+<body> 
+    <div class="container">
+        <div class="jumbotron" style="margin: 50px auto 10px; max-width: 840px;">
+            <img src="img/rule.jpg" alt="" class="pull-right img-rounded" style="width: 240px; height: auto;">
+            <h1 id="title" class="bigEntrance" style="font-size: 50px;">Cheesy Hans Gaming</h1>
+            <p class="lead">
+                Welcome to our TTT-Server. Have fun!<br>
+                <small>
+                    <ul style="line-height: 1.6;">
+                        <li>No ripping doors off the map. (This means you Splite)</li>
+                        <li>No Spamming</li>
+                        <li>No Ghosting!</li>
+                        <li>No Cheats/Exploits</li>
+                        <li>Do not crash the server</li>
+                    </ul>
+                    All used Workshop items can be found here:
+                    <br>
+                    <code>www.steamcommunity.com/sharedfiles/filedetails/?id=2893877910<b>xunocore</b></code><br>→ Servercontent DL (Link)
+                </small>
+            </p>
+
+        </div>
+    </div>
+    <script src="js/vendor/jquery-1.10.1.min.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
